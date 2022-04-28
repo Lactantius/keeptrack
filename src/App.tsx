d8a8b909d35e21e6758e8faa367d60f6981cf0a0
Gerard Keiser <gerardkeiser@gmail.com>
2022-04-28 11:06:02 -0500

    add Projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProjectsPage from './projects/ProjectsPage';
 
 function App() {
   return (
-    <blockquote cite="Benjamin Franklin">
-      Tell me and I forget.
-    </blockquote>
+    <div className="container">
+      <ProjectsPage />
+    </div>
   );
 }
 
